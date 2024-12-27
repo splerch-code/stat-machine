eaee5f3ca7765bbb32cd45d6806d9aea1e641fdf
= <=>
2024-12-27 12:01:44 -0600

    Updated banner. Made header fixed to top

--- src/components/main/home/Banner.jsx
+++ src/components/main/home/Banner.jsx
@@ -1,10 +1,10 @@
 const Banner = () => {
   return (
-    <div className="relative bg-gradient-to-r from-indigo-dye to-ash-gray-700 text-white py-16 px-8">
-      <div className="container mx-auto text-center">
+    <div className="relative bg-gradient-to-r from-indigo-dye to-ash-gray-700 text-alabaster py-16 px-8 rounded-lg m-4">
+      <div className="container mx-auto text-center max-w-3xl">
         <h1 className="text-4xl md:text-5xl font-medium mb-4">
           Welcome to <span className="font-ysabeau">Stat Machine</span> Sports
-          Data Analysis
+          Data Analytics
         </h1>
         <p className="text-lg md:text-xl mb-6">
           Explore articles and insights about trends in professional sports.
@@ -13,7 +13,7 @@ const Banner = () => {
           href="/about"
           className="bg-alabaster text-indigo-dye px-6 py-3 rounded-md text-lg font-semibold hover:bg-indigo-dye-300 hover:text-alabaster transition-colors duration-300"
         >
-          Learn More
+          About <span className="font-ysabeau">Stat Machine</span>
         </a>
       </div>
     </div>
